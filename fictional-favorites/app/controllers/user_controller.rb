849a0b5b08420c2bdda5f8fcb0fd3267469373c2
Brent Ransom <sadumedusa@gmail.com>
2019-04-10 17:01:13 +0000

    updated controller

--- fictional-favorites/app/controllers/user_controller.rb
+++ fictional-favorites/app/controllers/user_controller.rb
@@ -1,7 +1,11 @@
-class UsersController < ApplicationController
+class UserController < ApplicationController
 
   get '/signup' do
      erb :'/users/signup'
+   end
+
+   post '/signup' do
+
    end
 
 end
