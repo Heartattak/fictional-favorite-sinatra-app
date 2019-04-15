97e5ecb36931c0ca99e15bc3fbb0d9a1e39ad075
Brent Ransom <sadumedusa@gmail.com>
2019-04-15 16:36:49 +0000

    added login and updated controller

--- fictional-favorites/app/controllers/user_controller.rb
+++ fictional-favorites/app/controllers/user_controller.rb
@@ -5,7 +5,32 @@ class UserController < ApplicationController
    end
 
    post '/signup' do
-
+     if params[:username] != "" && params[:email] != "" && params[:password] != ""
+           @user = User.create(params)
+           @user.save
+           session[:user_id] = @user.id
+            redirect to '/lists/list'
+        else
+            redirect to '/users/signup'
+         end
    end
+
+   get '/login' do
+      if !logged_in?
+       erb :'/users/login'
+      else
+        redirect '/lists/list'
+      end
+    end
+
+    post '/login' do
+      @user = User.find_by(username: params[:username])
+       if @user && @user.authenticate(params[:password])
+           session[:user_id] = @user.id
+           redirect '/lists/list'
+           else
+              redirect '/users/signup'
+       end
+    end
 
 end
