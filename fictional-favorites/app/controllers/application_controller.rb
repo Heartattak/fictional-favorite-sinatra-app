61fc6b6f13505fb9714f06e0cf757866b705a968
Brent Ransom <sadumedusa@gmail.com>
2019-04-18 00:46:01 +0000

    updated app controller

--- fictional-favorites/app/controllers/application_controller.rb
+++ fictional-favorites/app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < Sinatra::Base
 
   configure do
     set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, "NMH"
     set :views, 'app/views'
   end
 
