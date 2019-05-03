e0f729a0f87013086e5597ef67fef29ba4d17f22
Brent Ransom <sadumedusa@gmail.com>
2019-05-03 20:03:36 +0000

    updated controller

--- fictional-favorites/app/controllers/user_controller.rb
+++ fictional-favorites/app/controllers/user_controller.rb
@@ -26,7 +26,7 @@ class UserController < ApplicationController
     post '/login' do
       @user = User.find_by(username: params[:username])
        if @user && @user.authenticate(params[:password])
-           session[:user_id] = @user.id
+         session[:user_id] = @user.id
            erb :'/lists/list'
            else
               erb :'/users/signup'
