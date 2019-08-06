92f1d91bf2450193ce352cbcba598b5886ba2735
Brent Ransom <sadumedusa@gmail.com>
2019-08-06 15:54:44 +0000

    updated char controller

--- fictional-favorites/app/controllers/char_controller.rb
+++ fictional-favorites/app/controllers/char_controller.rb
@@ -18,35 +18,48 @@ get '/chars/create_list' do
       @char.user = current_user
       @char.save
     redirect to '/chars/create_list'
+  else
+    redirect 'chars/list'
  end
 end
 
   get '/chars/:id/edit_char' do
       @char = Char.find_by_id(params[:id])
+      if @char.user_id == current_user.id
       erb :'/chars/edit_char'
+    end
   end
 
   patch '/chars/:id' do
       @char = Char.find_by_id(params[:id])
-
-    if params[:name] != "" && params[:origin] != "" && current_user
-      @char.update(name: params[:name])
-      @char.update(origin: params[:origin])
-      @char.save
-      redirect '/chars/list'
+      if @char.user_id == current_user.id
+        if params[:name] != "" && params[:origin] != "" && current_user
+          @char.update(name: params[:name])
+          @char.update(origin: params[:origin])
+          @char.save
+        redirect '/chars/list'
+      else
+        redirect "/chars/#{params[:id]}/edit_char"
+      end
     else
-      redirect "/chars/edit_char"
-  end
+      redirect 'chars/list'
+    end
 end
 
   get '/chars/:id/delete_char' do
       @char = Char.find_by_id(params[:id])
+      if @char.user_id == current_user.id
       erb :'/chars/delete_char'
+    end
   end
 
   delete "/chars/:id" do
       @char = Char.find_by_id(params[:id])
-      Char.destroy(params[:id]) if @char = current_user.chars
+      if @char.user_id == current_user.id
+        Char.destroy(params[:id])
+        redirect '/chars/list'
+    else
       redirect '/chars/list'
     end
+ end
  end
