4bbf33dc2412d55ab610f273e54f397ab1f5da92
Brent Ransom <sadumedusa@gmail.com>
2019-08-26 16:53:32 +0000

    updated controllers and erbs

--- fictional-favorites/app/controllers/char_controller.rb
+++ fictional-favorites/app/controllers/char_controller.rb
@@ -1,17 +1,15 @@
 class CharController < ApplicationController
 
   get '/chars/list' do
-    if @char = current_user.chars
+    redirect_if_not_logged_in
+    @chars = current_user.chars
     erb :'/chars/list'
-  else
-    redirect to '/'
   end
-end
 
 get '/chars/create_list' do
-  if @user.id == current_user.id
-   erb :'/chars/create_list'
- end
+  redirect_if_not_logged_in
+  erb :'/chars/create_list'
+end
 
  post '/create_list' do
     if params[:name] != "" && params[:origin] != "" && current_user
@@ -33,24 +31,25 @@ end
 
   patch '/chars/:id' do
       @char = Char.find_by_id(params[:id])
-      if @char.user_id == current_user.id
-        if params[:name] != "" && params[:origin] != "" && current_user
-          @char.update(name: params[:name])
-          @char.update(origin: params[:origin])
-          @char.save
+      if @char.user_id != current_user.id
+        redirect 'chars/list'
+      end
+      if params[:name] != "" && params[:origin] != "" && current_user
+        @char.update(name: params[:name])
+        @char.update(origin: params[:origin])
+        @char.save
         redirect '/chars/list'
       else
         redirect "/chars/#{params[:id]}/edit_char"
       end
-    else
-      redirect 'chars/list'
-    end
 end
 
   get '/chars/:id/delete_char' do
       @char = Char.find_by_id(params[:id])
       if @char.user_id == current_user.id
-      erb :'/chars/delete_char'
+        erb :'/chars/delete_char'
+      else
+        redirect '/chars/list'
     end
   end
 
